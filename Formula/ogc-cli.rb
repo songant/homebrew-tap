b100824cca133408e06cf9416167e08d355c27b7
songsong <songsong@saibit.com>
2024-10-21 12:04:17 +0000

    Update Formula with version ogc-cli-v0.4.2

--- Formula/ogc-cli.rb
+++ Formula/ogc-cli.rb
@@ -15,12 +15,12 @@
 
     if OS.linux? && Hardware::CPU.intel?
       url "https://github.com/saibit-tech/homebrew-tap/releases/download/ogc-cli-v0.4.2/ogc_cli-linux_amd64"
-      sha256 ""
+      sha256 "298b7dc642c7dc8a34ad63aa640c02955e5c4d5b392c05330adfcc620f442dab"
     end
 
     if OS.linux? && Hardware::CPU.arm?
       url "https://github.com/saibit-tech/homebrew-tap/releases/download/ogc-cli-v0.4.2/ogc_cli-linux_arm64"
-      sha256 "b315524eae0175f39a6fcad7a51dd0b165bc8742e31989c3b01bd2bcd9d42d6d"
+      sha256 ""
     end
 
     def install
