103cc69b24d9b9561049e1f90a0fe46b60b6c5eb
songsong <songsong@saibit.com>
2024-10-21 13:28:22 +0000

    Update Formula with version ogc-cli-v0.4.8

--- Formula/ogc-cli.rb
+++ Formula/ogc-cli.rb
@@ -1,25 +1,25 @@
   class OgcCli < Formula
     desc "ogc"
     homepage "https://github.com/saibit-tech/open-gpu-cloud"
-    version "ogc-cli-v0.4.7"
+    version "ogc-cli-v0.4.8"
 
     if OS.mac? && Hardware::CPU.intel?
-      url "https://github.com/saibit-tech/homebrew-tap/releases/download/ogc-cli-v0.4.7/ogc_cli-darwin_amd64"
+      url "https://github.com/saibit-tech/homebrew-tap/releases/download/ogc-cli-v0.4.8/ogc_cli-darwin_amd64"
       sha256 "4d8322c7cf1ffa042d4432dc482fd9a347ff889d1a96740c98a0192594a610ac"
     end
 
     if OS.mac? && Hardware::CPU.arm?
-      url "https://github.com/saibit-tech/homebrew-tap/releases/download/ogc-cli-v0.4.7/ogc_cli-darwin_arm64"
+      url "https://github.com/saibit-tech/homebrew-tap/releases/download/ogc-cli-v0.4.8/ogc_cli-darwin_arm64"
       sha256 "8c99063737cb8caafba89d82eda457f42985c6990a4ffa8e11cb29595cb27d97]"
     end
 
     if OS.linux? && Hardware::CPU.intel?
-      url "https://github.com/saibit-tech/homebrew-tap/releases/download/ogc-cli-v0.4.7/ogc_cli-linux_amd64"
+      url "https://github.com/saibit-tech/homebrew-tap/releases/download/ogc-cli-v0.4.8/ogc_cli-linux_amd64"
       sha256 "298b7dc642c7dc8a34ad63aa640c02955e5c4d5b392c05330adfcc620f442dab"
     end
 
     if OS.linux? && Hardware::CPU.arm?
-      url "https://github.com/saibit-tech/homebrew-tap/releases/download/ogc-cli-v0.4.7/ogc_cli-linux_arm64"
+      url "https://github.com/saibit-tech/homebrew-tap/releases/download/ogc-cli-v0.4.8/ogc_cli-linux_arm64"
       sha256 "b315524eae0175f39a6fcad7a51dd0b165bc8742e31989c3b01bd2bcd9d42d6d"
     end
 
